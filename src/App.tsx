8b8fa059c8961c94e6532e69f9dde7fc69cd632a
larissafiorini <larissa.fiorini@hotmail.com>
2024-10-18 16:13:29 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-//import { useEffect } from 'react';
-//import axios from 'axios';
 
 function App() {
   const apiUrl = import.meta.env.VITE_API_URL;
   const summary = `${apiUrl}/summary`
 
   console.log(apiUrl)
+  console.log(summary)
   
   fetch(summary).then((response) => {
     return response.json()
